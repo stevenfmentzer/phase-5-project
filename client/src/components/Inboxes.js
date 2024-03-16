bb031a758147d9d0249f4c2d6bc0352b859fe6b9
Steven Mentzer <stevenfmentzer@gmail.com>
2024-03-15 21:27:44 -0400

    Fix lines between InboxCards

--- client/src/components/Inboxes.js
+++ client/src/components/Inboxes.js
@@ -9,8 +9,12 @@ function Inboxes({ inboxes, onClick, selectedInboxIndex }) {
             {inboxes.map((inboxArray, index) => {
                 const inboxData = inboxArray[0]; // Accessing the inbox data from the first element
                 const isSelected = index === selectedInboxIndex; // Check if this inbox is selected
+                const isAfterSelected = index === selectedInboxIndex + 1; // Check if this inbox is directly after the selected one
+                const isFirstInList = index === 0; // Check if this inbox is the first in the list
+                const shouldDisplayLine = !isSelected && !isAfterSelected && !isFirstInList; // Determine if the line should be displayed
                 return (
                     <div className="card-container" key={inboxData.inbox_id} onClick={() => onClick(index)}>
+                        {shouldDisplayLine && <div className="horizontal-line"></div>} {/* Conditionally render the horizontal line */}
                         <InboxCard inboxData={inboxData} isSelected={isSelected} />
                     </div>
                 );
