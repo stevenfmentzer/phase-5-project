a7dd391d0768c1de53058fb72e6fed25476c26a0
Steven Mentzer <stevenfmentzer@gmail.com>
2024-03-15 21:28:10 -0400

    minor updates

--- client/src/components/InboxCard.js
+++ client/src/components/InboxCard.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Icon, Card } from 'semantic-ui-react';
+import { Card } from 'semantic-ui-react';
 import '../styling/InboxCard.css'; 
 
 function InboxCard({ inboxData, isSelected }) {
     return (
-        <div className={`card-container ${isSelected ? 'selected' : ''}`}> {/* Apply the selected class conditionally */}
+        <div className={`card-container ${isSelected ? 'selected' : ''}`}> 
             <Card>
                 <Card.Content>
-                    {/* Apply the CSS class conditionally */}
                     <Card.Header className={`inbox-name ${isSelected ? 'selected' : ''}`}>
                         {`${inboxData.contact_user.first_name} ${inboxData.contact_user.last_name}`}
                     </Card.Header>
-                    {/* Display the last message */}
                     <p className="inbox-description">{inboxData.last_message_body}</p>
                 </Card.Content>
             </Card>
